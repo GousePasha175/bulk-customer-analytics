ee9b3d3c489a797a4889e0e306ca5764b2d6a3a4
GousePasha175 <sh.gp43@gmail.com>
2026-07-02 00:56:32 +0530

    Update Bulk Analytics.py

--- pages/Bulk Analytics.py
+++ pages/Bulk Analytics.py
@@ -20,7 +20,7 @@ def _render_nav():
                     ("pages/Bulk Analytics.py|pages/*[Bb]ulk*.py","\U0001f4ca Bulk Customer Analytics"), 
                     ("pages/Delivery Productivity.py|pages/*[Dd]elivery*.py", "\U0001f4e6 Delivery Productivity")
                     ("pages/1_Digital_Transactions.py|pages/*[Dd]igital*.py","\U0001f4bb Digital Transactions"),
-                    ("pages/POSB Daily Report.py|pages/*[Pp][Oo][Ss][Bb]*.py","\U0001f4ee POSB Daily Report"),]:
+                    ("pages/POSB Daily Report.py|pages/*[Pp][Oo][Ss][Bb]*.py","\U0001f4ee POSB Daily Report")]:
                     
         hits=[]
         for p in pat.split("|"): hits+=_glob.glob(p)
