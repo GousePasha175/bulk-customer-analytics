9c91ccb0d9e00dcfb1efbdea7de999beee149527
GousePasha175 <sh.gp43@gmail.com>
2026-07-02 00:55:12 +0530

    Update Bulk Analytics.py

--- pages/Bulk Analytics.py
+++ pages/Bulk Analytics.py
@@ -16,8 +16,7 @@ def _render_nav():
         </div>""", unsafe_allow_html=True)
     st.sidebar.page_link("Analytics_Excel.py", label="\U0001f512 Home")
     st.sidebar.markdown("<hr style='margin:8px 0 12px 0;'>",unsafe_allow_html=True)
-    for pat,lbl in [
-                    ("pages/AEBAS_Monitoring.py|pages/*[Aa][Ee][Bb][Aa][Ss]*.py","\U0001f91a AEBAS Monitoring"),
+    for pat,lbl in [("pages/AEBAS_Monitoring.py|pages/*[Aa][Ee][Bb][Aa][Ss]*.py","\U0001f91a AEBAS Monitoring"),
                     ("pages/Bulk Analytics.py|pages/*[Bb]ulk*.py","\U0001f4ca Bulk Customer Analytics"), 
                     ("pages/Delivery Productivity.py|pages/*[Dd]elivery*.py", "\U0001f4e6 Delivery Productivity")]:            
                     ("pages/1_Digital_Transactions.py|pages/*[Dd]igital*.py","\U0001f4bb Digital Transactions"),
