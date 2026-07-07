52281deee43a9644c608e4025ec64eaa8c4980e2
GousePasha175 <sh.gp43@gmail.com>
2026-07-07 21:56:02 +0530

    Update AEBAS_Monitoring.py

--- pages/AEBAS_Monitoring.py
+++ pages/AEBAS_Monitoring.py
@@ -815,8 +815,8 @@ summary["% AEBAS"] = (summary["Marked"] / summary["Total"] * 100).round(2)
 summary = summary.sort_values(by=["% AEBAS", "Marked"],ascending=[False, False]).reset_index(drop=True)
 summary.insert(0, "Sl.", range(1, len(summary)+1))
 summary.columns = ["Sl.","Division/Unit","Total","Marked","Not Marked","% AEBAS"]
-summary.insert(0, "Sl.", range(1, len(summary) + 1))
-summary.columns = ["Sl.", "Division/Unit", "Total", "Marked", "Not Marked", "% AEBAS"]
+#summary.insert(0, "Sl.", range(1, len(summary) + 1))
+#summary.columns = ["Sl.", "Division/Unit", "Total", "Marked", "Not Marked", "% AEBAS"]
 
 tot_t = summary["Total"].sum()
 tot_m = summary["Marked"].sum()
